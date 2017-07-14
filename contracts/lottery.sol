eb36c0a2cdc66918c2e65a5651d12095bbb2ecf6
CrazybutSolid <wachichornia@gmail.com>
2017-07-14 14:22:01 +0200

    added future blockchain fix

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -1,106 +1,29 @@
 pragma solidity ^0.4.11;
 
 // Contract that:
-//      Lets anyone bet with 1 ether
-//      When it reaches 5 bets, it chooses a gambler at random and sends the 5 ethers received
+//      Lets anyone bet with x ethers
+//      When it reaches y bets, it chooses a gambler at random and sends z% of the ethers received. other v% goes to GiveDirectly
 
 
-library SafeMath {
-  function mul(uint256 a, uint256 b) internal returns (uint256) {
-    uint256 c = a * b;
-    assert(a == 0 || c / a == b);
-    return c;
-}
-
-function div(uint256 a, uint256 b) internal returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return c;
-}
-
-function sub(uint256 a, uint256 b) internal returns (uint256) {
-    assert(b <= a);
-    return a - b;
-}
-
-function add(uint256 a, uint256 b) internal returns (uint256) {
-    uint256 c = a + b;
-    assert(c >= a);
-    return c;
-}
-}
-
-/**
- * @title PullPayment
- * @dev Base contract supporting async send for pull payments. Inherit from this
- * contract and use asyncSend instead of send.
- */
- contract PullPayment {
-  using SafeMath for uint256;
-
-  mapping(address => uint256) public payments;
-  uint256 public totalPayments;
-
-  /**
-  * @dev Called by the payer to store the sent amount as credit to be pulled.
-  * @param dest The destination address of the funds.
-  * @param amount The amount to transfer.
-  */
-  function asyncSend(address dest, uint256 amount) internal {
-    payments[dest] = payments[dest].add(amount);
-    totalPayments = totalPayments.add(amount);
-}
-
-  /**
-  * @dev withdraw accumulated balance, called by payee.
-  */
-  function withdrawPayments() {
-    address payee = msg.sender;
-    uint256 payment = payments[payee];
-
-    if (payment == 0) {
-      throw;
-  }
-
-  if (this.balance < payment) {
-      throw;
-  }
-
-  totalPayments = totalPayments.sub(payment);
-  payments[payee] = 0;
-
-  if (!payee.send(payment)) {
-      throw;
-  }
-}
-}
-
-contract Lottery is PullPayment {
+contract Lottery {
 
 
 
     mapping(uint => address) public gamblers;// A mapping to store ethereum addresses of the gamblers
-    mapping(uint => address) public winners;// A mapping to store ethereum addresses of the winners
-    uint8 winner_count; //keep track of how many people are signed up.
     uint8 public player_count; //keep track of how many people are signed up.
     uint public ante; //how big is the bet per person (in ether)
     uint8 public required_number_players; //how many sign ups trigger the lottery
+    uint8 public next_round_players; //how many sign ups trigger the lottery
     uint random; //random number
     uint public winner_percentage; // how much does the winner get (in percentage)
-    address public winner; //the last winner of the lottery
     address owner; // owner of the contract
-    uint80 constant None = uint80(0); //null constant
-    uint total_payout; // the payout 
-    uint givedirectly_payout; // the payout 
-    uint winner_payout; // the payout 
+    uint bet_blocknumber; //block number on the moment the required number of players signed up
 
 
     //constructor
     function Lottery(){
         owner = msg.sender;
         player_count = 0;
-        winner_count = 0;
         ante = 0.2 ether;
         required_number_players = 5;
         winner_percentage = 80;
@@ -110,15 +33,25 @@ contract Lottery is PullPayment {
     function changeParameters(uint newAnte, uint8 newNumberOfPlayers, uint newWinnerPercentage) {
         // Only the creator can alter this
         if (msg.sender == owner) {
-         if (newAnte != None) {
+         if (newAnte != uint80(0)) {
             ante = newAnte;
         }
-        if (newNumberOfPlayers != None) {
+        if (newNumberOfPlayers != uint80(0)) {
             required_number_players = newNumberOfPlayers;
         }
-        if (newWinnerPercentage != None) {
+        if (newWinnerPercentage != uint80(0)) {
             winner_percentage = newWinnerPercentage;
         }
+    }
+}
+
+function refund() {
+    if (msg.sender == owner) {
+        while (this.balance > ante) {
+                gamblers[player_count].transfer(ante);
+                player_count -=1;    
+            }
+            gamblers[1].transfer(this.balance);
     }
 }
 // announce the winner with an event
@@ -127,13 +60,9 @@ event Announce_winner(
     address indexed _to,
     uint _value
     );
-// conditional to make sure participants send the right ante. haven't been able to make this work yet
-//modifier only_right_ante(uint x) {
-  //  if (balances[msg.value] == x) _;
-  //}
-  
-  // function when someone gambles a.k.a sends ether to the contract
-  function () payable {
+
+// function when someone gambles a.k.a sends ether to the contract
+function () payable {
     // No arguments are necessary, all
     // information is already part of
     // the transaction. The keyword payable
@@ -142,7 +71,6 @@ event Announce_winner(
 
     // If the bet is not equal to the ante, send the
     // money back.
-    //require(msg.value == ante);
     if(msg.value != ante) throw; // give it back, revert state changes, abnormal stop
     player_count +=1;
 
@@ -150,37 +78,26 @@ event Announce_winner(
     
     // when we have enough participants
     if (player_count == required_number_players) {
-        // pick a random number between 1 and 5
-        random = uint(block.blockhash(block.number-1))%5 + 1;
-        // save the last winner
-        winner = gamblers[random];
-        // make payment available for the winner to withdraw
-        total_payout = ante*required_number_players;
-        winner_payout = total_payout*winner_percentage/100;
-        givedirectly_payout = total_payout - winner_payout;
-
-        // more secure way to move funds: make the winners withdraw them. Will implement later.
-        //asyncSend(gamblers[random],winner_payout);
-        
-        // previous method to directly transfer winnings
-        gamblers[random].transfer(winner_payout);
-        winner_count +=1;
-        winners[winner_count] = gamblers[random];
-        // launch the event with the announce
-        Announce_winner(this,gamblers[random],winner_payout);
-        // sends 0.2 ethers to GiveDirectly in production
-        // 0x512c3FCccdEFDa5D58E188b1AF39893e5D147aA3.transfer(givedirectly_payout);
-        // Fake Give Directly address (Ropsten)
-        0xd60406B842Ba7bCA8E83aF189e2A1bc96b24072B.transfer(givedirectly_payout);
-        //reset the counter
-        player_count = 0;
-        // reset the addresses
-        gamblers[1] = 0;
-        gamblers[2] = 0;
-        gamblers[3] = 0;
-        gamblers[4] = 0;
-        gamblers[5] = 0;
-
+        bet_blocknumber=block.number;
     }
+    if (player_count > required_number_players) {
+        if (block.number>bet_blocknumber){
+            // pick a random number between 1 and 5
+            random = uint(block.blockhash(block.number-1))%required_number_players + 1;
+            // more secure way to move funds: make the winners withdraw them. Will implement later.
+            //asyncSend(gamblers[random],winner_payout);
+            gamblers[random].transfer(ante*required_number_players*winner_percentage/100);
+            0xd60406B842Ba7bCA8E83aF189e2A1bc96b24072B.transfer(ante*required_number_players - ante*required_number_players*winner_percentage/100);
+            // move the gamblers who have joined the lottery but did not participate on this draw down on the mapping structure for next bets
+            next_round_players = player_count-required_number_players;
+            while (player_count > required_number_players) {
+                gamblers[player_count-required_number_players] = gamblers[player_count];
+                player_count -=1;    
+            }
+            player_count = next_round_players;
+        }
+        else throw;
+    }
+    
 }
 }
